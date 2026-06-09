2a86e4a556513d401f9838742ebe6874af05edea
SRINITHYA T <139114296+SRINITHYA04@users.noreply.github.com>
2026-06-09 03:47:53 +0000

    Initial setup done

--- Project_FastAPI/app.py
+++ Project_FastAPI/app.py
@@ -7,57 +7,57 @@ app = FastAPI(title="FastAPI Crash course")
 def root():
     return{"messege":"Hello FastAPI"}
 
-@app.get("/Sri")
-def root():
-    return{"messege":"Hello Srinithya"}
+    @app.get("/Sri")
+    def root():
+        return{"messege":"Hello Srinithya"}
 
-# sending single parameter
-@app.get("/user/{user_id}")
-def user_name(user_id:int):
-    return{"user_id":user_id, "type":str(type(user_id))}
+        # sending single parameter
+        @app.get("/user/{user_id}")
+        def user_name(user_id:int):
+            return{"user_id":user_id, "type":str(type(user_id))}
 
-# sending multiple parameter
+            # sending multiple parameter
 
-@app.get("/search")
-def search(q: str, page: int=1, limit: int=10):
-    return{"q":q, "page": page, "limit":limit}
+            @app.get("/search")
+            def search(q: str, page: int=1, limit: int=10):
+                return{"q":q, "page": page, "limit":limit}
 
-class userCreate(BaseModel):
-    name: str
-    email: str
-    age:int | None = None
+                class userCreate(BaseModel):
+                    name: str
+                        email: str
+                            age:int | None = None
 
-@app.post("/users")
-def create_user(payload: userCreate):
-    return{"created": True, "User": payload.model_dump()}
+                            @app.post("/users")
+                            def create_user(payload: userCreate):
+                                return{"created": True, "User": payload.model_dump()}
 
-# curl -X POST http://127.0.0.1:8000/users \
-# -H "Content-Type: application/json" \
-# -d '{"name":"Sri","email":"Sri@demo.com","age":22}'
-
-
-class ProductCreate(BaseModel):
-    name: str = Field(min_length=2, max_length=50)
-    price: float = Field(ge=0)
-    stock: int= Field(ge=0)
-
-@app.post("/products")
-def create_product(p: ProductCreate):
-    return p.model_dump()
+                                # curl -X POST http://127.0.0.1:8000/users \
+                                # -H "Content-Type: application/json" \
+                                # -d '{"name":"Sri","email":"Sri@demo.com","age":22}'
 
 
-# curl -X POST http://127.0.0.1:8000/products \
-# -H "Content-Type: application/json" \
-# -d '{"name":"pen","price":10.5,"stock":100}'
+                                class ProductCreate(BaseModel):
+                                    name: str = Field(min_length=2, max_length=50)
+                                        price: float = Field(ge=0)
+                                            stock: int= Field(ge=0)
 
-@app.post("/items", status_code=status.HTTP_226_IM_USED)
-def create_item():
-    return {"created": True}
+                                            @app.post("/products")
+                                            def create_product(p: ProductCreate):
+                                                return p.model_dump()
 
-FAKE_DB={1:"Sri", 2:"Varsh"}
 
-@app.get("/user/db/{user_id}")
-def User_from_DB(user_id: int):
-    if user_id not in FAKE_DB:
-        raise HTTPException(status_code =404, detail="User not found")
-    return {"user_id": user_id, "Name":FAKE_DB[user_id]}
+                                                # curl -X POST http://127.0.0.1:8000/products \
+                                                # -H "Content-Type: application/json" \
+                                                # -d '{"name":"pen","price":10.5,"stock":100}'
+
+                                                @app.post("/items", status_code=status.HTTP_226_IM_USED)
+                                                def create_item():
+                                                    return {"created": True}
+
+                                                    FAKE_DB={1:"Sri", 2:"Varsh"}
+
+                                                    @app.get("/user/db/{user_id}")
+                                                    def User_from_DB(user_id: int):
+                                                        if user_id not in FAKE_DB:
+                                                                raise HTTPException(status_code =404, detail="User not found")
+                                                                    return {"user_id": user_id, "Name":FAKE_DB[user_id]}
